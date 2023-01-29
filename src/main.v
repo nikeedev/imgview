77c28e0046172492512f5ea37a993e2aa530b26b
nikeedev <nikita.gon.dev@gmail.com>
2023-01-29 13:32:13 +0100

    updates

--- src/main.v
+++ src/main.v
@@ -5,26 +5,23 @@ import gx
 import os
 import term
 
-
 struct App {
 mut:
-	ctx    &gg.Context = unsafe { nil }
-	img  gg.Image
+	ctx       &gg.Context = unsafe { nil }
+	img       gg.Image
 	file_name string
 }
 
 const (
-	win_width = 800
+	win_width  = 800
 	win_height = 600
 )
 
 fn main() {
-
 	if os.args.len < 2 {
-		println(term.cyan("Usage: imgview <filename>.png/.jpg/.jpeg\n"))
+		println(term.cyan('Usage: imgview <filename>.png/.jpg/.jpeg\n'))
 		return
-	}
-	else {
+	} else {
 		mut file_name := os.args[1]
 
 		if !os.exists(file_name) {
@@ -33,7 +30,7 @@ fn main() {
 		}
 
 		mut app := &App{
-			ctx: 0,
+			ctx: 0
 			file_name: file_name
 		}
 
@@ -42,27 +39,22 @@ fn main() {
 			width: win_width
 			height: win_height
 			create_window: true
-			window_title: "imgview - ${file_name}"
+			window_title: 'imgview - ${file_name}'
 			frame_fn: frame
 			user_data: app
 			init_fn: init
 		)
 
 		app.ctx.run()
-
 	}
 }
 
-fn init(mut app &App) {
-
+fn init(mut app App) {
 	app.img = app.ctx.create_image(os.resource_abs_path(app.file_name))
-
 }
 
 fn (app &App) draw() {
-
 	app.ctx.draw_image(0, 0, app.img.width, app.img.height, app.img)
-
 }
 
 fn frame(app &App) {
